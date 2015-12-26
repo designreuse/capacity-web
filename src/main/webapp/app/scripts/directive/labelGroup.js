d3f7d96410f8235153d81bbd20b896612193c985
Christoph Brill <egore911@gmail.com>
2015-12-26 01:42:46 +0100

    Add directive to add labled elements
    Will automatically link the 'for' attribute to the first input element

---
+++ src/main/webapp/app/scripts/directive/labelGroup.js
@@ -0,0 +1,27 @@
+'use strict';
+
+angular.module('capacityApp')
+	.directive('labelGroup', function() {
+		var autoincrementId = 0;
+		
+		return {
+			templateUrl: 'app/scripts/directive/labelGroup.html',
+			restrict: 'E',
+			replace: true,
+			transclude: true,
+			scope: {
+				label: '@'
+			},
+			link: function(scope, element, attrs, formController) {
+				var controls = element.find(':input');
+				if (controls.length > 0) {
+					var id = controls[0].id;
+					if (!id) {
+						id = 'control_id_' + (autoincrementId++);
+						controls[0].id = id;
+					}
+					scope.forControl = id;
+				}
+			}
+		}
+	});
