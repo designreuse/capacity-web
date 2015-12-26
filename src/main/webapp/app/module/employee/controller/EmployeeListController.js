c81c500fd50a14105b637e738663f3ced8972f7d
Christoph Brill <egore911@gmail.com>
2015-12-26 21:12:34 +0100

    Move searching to the root scope

--- src/main/webapp/app/module/employee/controller/EmployeeListController.js
+++ src/main/webapp/app/module/employee/controller/EmployeeListController.js
@@ -1,13 +1,17 @@
 'use strict';
 
 angular.module('capacityApp')
-	.controller('EmployeeListController', ['$scope', '$location', 'Employee', function ($scope, $location, Employee) {
+	.controller('EmployeeListController', ['$scope', '$location', '$rootScope', 'Employee', function ($scope, $location, $rootScope, Employee) {
 		Employee.query(function(employees) {
-	        $scope.employees = employees;
-	        
-	        $scope.add = function() {
-	        	$location.path('/employees/new');
-        	};
-	    });
+			$scope.employees = employees;
+
+			$scope.add = function() {
+				$location.path('/employees/new');
+			};
+		});
+
+		$scope.searchFilter = function(string) {
+			$rootScope.search = string;
+		};
 	}]
 );
