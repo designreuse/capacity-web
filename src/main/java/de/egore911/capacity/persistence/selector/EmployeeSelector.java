51273c928e0990bbd039a8219368b157b52a2df4
Christoph Brill <egore911@gmail.com>
2015-12-28 11:59:45 +0100

    Allow EmployeeSelector to load multiple employees by their IDs

--- src/main/java/de/egore911/capacity/persistence/selector/EmployeeSelector.java
+++ src/main/java/de/egore911/capacity/persistence/selector/EmployeeSelector.java
@@ -1,22 +1,27 @@
 package de.egore911.capacity.persistence.selector;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
+import org.apache.commons.collections4.CollectionUtils;
+
 import de.egore911.capacity.persistence.model.EmployeeEntity;
 import de.egore911.capacity.persistence.model.EmployeeEntity_;
+import de.egore911.capacity.persistence.model.IntegerDbObject_;
 import de.egore911.persistence.selector.AbstractSelector;
 
 public class EmployeeSelector extends AbstractSelector<EmployeeEntity> {
 
 	private static final long serialVersionUID = -4834238226605804927L;
-	
-	private Integer id;
-	
+
+	private Collection<Integer> ids;
+
 	@Override
 	protected Class<EmployeeEntity> getEntityClass() {
 		return EmployeeEntity.class;
@@ -25,16 +30,21 @@ public class EmployeeSelector extends AbstractSelector<EmployeeEntity> {
 	@Override
 	protected List<Predicate> generatePredicateList(CriteriaBuilder builder, Root<EmployeeEntity> from) {
 		List<Predicate> predicates = new ArrayList<>();
-		
-		if (id != null) {
-			predicates.add(builder.equal(from.get(EmployeeEntity_.id), id));
+
+		if (CollectionUtils.isNotEmpty(ids)) {
+			predicates.add(from.get(IntegerDbObject_.id).in(ids));
 		}
 		
 		return predicates;
 	}
-	
+
 	public EmployeeSelector withId(Integer id) {
-		this.id = id;
+		this.ids = Collections.singleton(id);
+		return this;
+	}
+
+	public EmployeeSelector withIds(Collection<Integer> ids) {
+		this.ids = ids;
 		return this;
 	}
 
