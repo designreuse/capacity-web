45c9d8cabaadf9da0ac3e36aea7292fb100b2cbb
Christoph Brill <egore911@gmail.com>
2015-12-28 12:06:02 +0100

    Add color picker to employees

--- src/main/webapp/app/scripts/app.js
+++ src/main/webapp/app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('capacityApp', ['ngResource', 'ngRoute', 'ngTagsInput', 'ui.calendar', 'highcharts-ng'])
+angular.module('capacityApp', ['ngResource', 'ngRoute', 'ngTagsInput', 'ui.calendar', 'highcharts-ng', 'colorpicker.module'])
 	.controller('MainCtrl', ['$scope', '$location', function($scope, $location) {
 		$scope.currentModule = function() {
 			var currentPath = $location.path();
