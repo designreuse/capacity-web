73e34fd8fb1cf7e55253411c905075253fb7a73f
Christoph Brill <egore911@gmail.com>
2015-12-28 12:06:55 +0100

    Fix adding the 'add' method only once to the scope
    Not that it did any harm, but it wasn't meant to be there

--- src/main/webapp/app/module/employee/controller/EmployeeListController.js
+++ src/main/webapp/app/module/employee/controller/EmployeeListController.js
@@ -4,11 +4,11 @@ angular.module('capacityApp')
 	.controller('EmployeeListController', ['$scope', '$location', '$rootScope', 'Employee', function ($scope, $location, $rootScope, Employee) {
 		Employee.query(function(employees) {
 			$scope.employees = employees;
-
-			$scope.add = function() {
-				$location.path('/employees/new');
-			};
 		});
+
+		$scope.add = function() {
+			$location.path('/employees/new');
+		};
 
 		$scope.searchFilter = function(string) {
 			$rootScope.search = string;
