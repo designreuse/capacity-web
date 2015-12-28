eebb4994135dc842667ede1f6f5d7885683a561f
Christoph Brill <egore911@gmail.com>
2015-12-28 12:03:10 +0100

    Implement filtering to employees to ones with valid contracts in a given time range

--- src/main/java/de/egore911/capacity/persistence/selector/EmployeeSelector.java
+++ src/main/java/de/egore911/capacity/persistence/selector/EmployeeSelector.java
@@ -10,7 +10,9 @@ import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.apache.commons.collections4.CollectionUtils;
+import org.joda.time.LocalDate;
 
+import de.egore911.capacity.persistence.model.ContractEntity_;
 import de.egore911.capacity.persistence.model.EmployeeEntity;
 import de.egore911.capacity.persistence.model.EmployeeEntity_;
 import de.egore911.capacity.persistence.model.IntegerDbObject_;
@@ -21,6 +23,8 @@ public class EmployeeSelector extends AbstractSelector<EmployeeEntity> {
 	private static final long serialVersionUID = -4834238226605804927L;
 
 	private Collection<Integer> ids;
+	private LocalDate contractRangeStartDate;
+	private LocalDate contractRangeEndDate;
 
 	@Override
 	protected Class<EmployeeEntity> getEntityClass() {
@@ -34,12 +38,55 @@ public class EmployeeSelector extends AbstractSelector<EmployeeEntity> {
 		if (CollectionUtils.isNotEmpty(ids)) {
 			predicates.add(from.get(IntegerDbObject_.id).in(ids));
 		}
-		
+
+		if (contractRangeStartDate != null && contractRangeEndDate != null) {
+			predicates.add(
+				builder.or(
+					// Case 1: The query range starts before the contract starts and ends after the contract is started
+					builder.and(
+						builder.lessThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.start), contractRangeStartDate),
+						builder.greaterThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.start), contractRangeEndDate)
+					),
+					// Case 2: The query range starts before the contract ends and ends after the contract is ended
+					builder.and(
+						builder.lessThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.end), contractRangeStartDate),
+						builder.greaterThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.end), contractRangeEndDate)
+					),
+					// Case 3: The query range starts after the contract starts and ends before the contract is ended
+					builder.and(
+						builder.lessThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.start), contractRangeStartDate),
+						builder.greaterThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.end), contractRangeEndDate)
+					),
+					// Case 4: The contract has neither a start nor an end date
+					builder.and(
+						from.get(EmployeeEntity_.contract).get(ContractEntity_.start).isNull(),
+						from.get(EmployeeEntity_.contract).get(ContractEntity_.end).isNull()
+					),
+					// Case 5: The contract starts before our query range and the contract does not have an end date
+					builder.and(
+						builder.lessThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.start), contractRangeStartDate),
+						from.get(EmployeeEntity_.contract).get(ContractEntity_.end).isNull()
+					),
+					// Case 6: The contract ends after our query range and the contract does not have a start date
+					builder.and(
+						builder.greaterThanOrEqualTo(from.get(EmployeeEntity_.contract).get(ContractEntity_.end), contractRangeEndDate),
+						from.get(EmployeeEntity_.contract).get(ContractEntity_.start).isNull()
+					)
+				)
+			);
+		}
+
 		return predicates;
 	}
 
 	public EmployeeSelector withId(Integer id) {
 		this.ids = Collections.singleton(id);
+		return this;
+	}
+
+	public EmployeeSelector withActiveContract(LocalDate contractRangeStartDate, LocalDate contractRangeEndDate) {
+		this.contractRangeStartDate = contractRangeStartDate;
+		this.contractRangeEndDate = contractRangeEndDate;
 		return this;
 	}
 
