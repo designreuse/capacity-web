2808b7b56aa53320c80dfdd143b97a68d88b23bf
Christoph Brill <egore911@gmail.com>
2016-01-26 00:10:41 +0100

    Try to make the calendar more useful

--- src/main/webapp/app/module/calendar/controller/CalendarController.js
+++ src/main/webapp/app/module/calendar/controller/CalendarController.js
@@ -1,27 +1,43 @@
 'use strict';
 
 angular.module('capacityApp')
-	.controller('CalendarController', ['$scope', '$http', function($scope, $http) {
+	.controller('CalendarController', ['$scope', '$http', 'uiCalendarConfig', function($scope, $http, uiCalendarConfig) {
 
 		$scope.calendars = [
-			{ id: 'holidays', name: 'Holidays' },
-			{ id: 'employees', name: 'Employees' }
+			{ id: 'holidays', name: 'Holidays', selected: true },
+			{ id: 'employees', name: 'Employees', selected: true }
 		];
+
+		var publishEvents = function(index) {
+			if ($scope.calendars[index].selected) {
+				$scope.eventSources[index] = $scope.allEvents[index];
+			} else {
+				$scope.eventSources[index] = [];
+			}
+		};
+
+		$scope.publishAllEvents = function() {
+			publishEvents(0);
+			publishEvents(1);
+		};
 
 		$scope.start = moment().startOf('month').format('YYYY-MM-DD');
 		$scope.end = moment().endOf('month').format('YYYY-MM-DD');
 		$scope.eventSources = [];
+		$scope.allEvents = [[],[]];
 		$http.get('rest/calendar/events/holidays?start=' + $scope.start + '&end=' + $scope.end).then(function(response) {
 			response.data.forEach(function(element, index) {
 				element.className = 'holidays';
 			});
-			$scope.eventSources.push(response.data);
+			$scope.allEvents[0] = response.data;
+			publishEvents(0);
 		});
 		$http.get('rest/calendar/events/absences?start=' + $scope.start + '&end=' + $scope.end).then(function(response) {
 			response.data.forEach(function(element, index) {
 				element.className = 'employees';
 			});
-			$scope.eventSources.push(response.data);
+			$scope.allEvents[1] = response.data;
+			publishEvents(1);
 		});
 		// Ideally we would do this: $scope.eventSources = [ 'rest/calendar/events' ];
 	}]);
