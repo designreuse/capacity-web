a7a38b3dc98a640f2252b00ec6bf0e97984913f8
Christoph Brill <egore911@gmail.com>
2016-01-26 00:07:24 +0100

    Simplify the logic in the absence selector

--- src/main/java/de/egore911/capacity/persistence/selector/AbsenceSelector.java
+++ src/main/java/de/egore911/capacity/persistence/selector/AbsenceSelector.java
@@ -35,44 +35,29 @@ public class AbsenceSelector extends AbstractSelector<AbsenceEntity> {
 		if (employeeId != null) {
 			predicates.add(builder.equal(from.get(AbsenceEntity_.employee).get(IntegerDbObject_.id), employeeId));
 		}
-		
+
 		if (startInclusive != null && endInclusive != null) {
 			predicates.add(
 				builder.or(
-					// Case 1: The query range starts before the absence starts and ends after the absence is started
+					// Absence starts before query range begin and ends after query range begin
 					builder.and(
 						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.start), startInclusive),
-						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.start), endInclusive)
+						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.end), startInclusive)
 					),
-					// Case 2: The query range starts before the absence ends and ends after the absence is ended
+					// Absence starts before query range end and ends after query range end
 					builder.and(
-						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.end), startInclusive),
+						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.start), endInclusive),
 						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.end), endInclusive)
 					),
-					// Case 3: The query range starts after the absence starts and ends before the absence is ended
-					builder.and(
-						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.start), startInclusive),
-						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.end), endInclusive)
-					),
-					// Case 4: The query range starts after the absence starts and ends before the absence is ended
+					// Absence starts in query range
 					builder.and(
 						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.start), startInclusive),
+						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.start), endInclusive)
+					),
+					// Absence ends in query range
+					builder.and(
+						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.end), startInclusive),
 						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.end), endInclusive)
-					),
-					// Case 5: The absence has neither a start nor an end date
-					builder.and(
-						from.get(AbsenceEntity_.start).isNull(),
-						from.get(AbsenceEntity_.end).isNull()
-					),
-					// Case 6: The absence starts before our query range and the absence does not have an end date
-					builder.and(
-						builder.lessThanOrEqualTo(from.get(AbsenceEntity_.start), startInclusive),
-						from.get(AbsenceEntity_.end).isNull()
-					),
-					// Case 7: The absence ends after our query range and the absence does not have a start date
-					builder.and(
-						builder.greaterThanOrEqualTo(from.get(AbsenceEntity_.end), endInclusive),
-						from.get(AbsenceEntity_.start).isNull()
 					)
 				)
 			);
