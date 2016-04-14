97b544f2735c78d93b9a88af4663c2a43304d89a
Christoph Brill <egore911@gmail.com>
2016-04-14 12:22:18 +0200

    Add commented test case for a test requiring the entity manager

---
+++ src/test/java/de/egore911/capacity/ui/CalendarServiceTest.java
@@ -0,0 +1,25 @@
+package de.egore911.capacity.ui;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import de.egore911.capacity.ui.rest.CalendarService;
+import de.egore911.capacity.ui.rest.JerseyConfig;
+
+public class CalendarServiceTest extends AbstractUiTest {
+
+	@Override
+	protected JerseyConfig configure() {
+		JerseyConfig application = super.configure();
+		application.register(CalendarService.class);
+		return application;
+	}
+
+	@Test
+	@Ignore
+	public void test() {
+		String hello = target("calendar/holidays.ics").request().get(String.class);
+		System.err.println(hello);
+	}
+
+}
