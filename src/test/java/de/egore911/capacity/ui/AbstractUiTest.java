b949f663cb4912ecc09282cad0cae61fe4cf54b9
Christoph Brill <egore911@gmail.com>
2016-04-14 12:21:30 +0200

    Add initial attempt to add a servlet filter
    Sadly, not working yet.

--- src/test/java/de/egore911/capacity/ui/AbstractUiTest.java
+++ src/test/java/de/egore911/capacity/ui/AbstractUiTest.java
@@ -1,10 +1,14 @@
 package de.egore911.capacity.ui;
 
+import org.glassfish.jersey.test.DeploymentContext;
 import org.glassfish.jersey.test.JerseyTest;
+import org.glassfish.jersey.test.ServletDeploymentContext;
 import org.junit.BeforeClass;
 
 import de.egore911.capacity.OnceInit;
+import de.egore911.capacity.ui.filter.EntityManagerFilter;
 import de.egore911.capacity.ui.rest.JerseyConfig;
+import de.egore911.capacity.ui.rest.JodaParameterConverterProvider;
 
 public abstract class AbstractUiTest extends JerseyTest {
 
@@ -16,7 +20,14 @@ public abstract class AbstractUiTest extends JerseyTest {
 
 	@Override
 	protected JerseyConfig configure() {
-		return new JerseyConfig();
+		JerseyConfig application = new JerseyConfig();
+		application.register(JodaParameterConverterProvider.class);
+		return application;
+	}
+
+	@Override
+	protected DeploymentContext configureDeployment() {
+		return ServletDeploymentContext.builder(configure()).addFilter(EntityManagerFilter.class, "EntityManagerFilter").build();
 	}
 
 }
