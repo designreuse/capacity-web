7c446422372d2ab098dc4c2c27c1ebcc338b29c1
Christoph Brill <egore911@gmail.com>
2016-04-17 22:46:57 +0200

    Add CRUD functionality for employees

---
+++ src/test/java/de/egore911/capacity/ui/EmployeeCRUDTest.java
@@ -0,0 +1,51 @@
+package de.egore911.capacity.ui;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.Collections;
+import java.util.UUID;
+
+import de.egore911.capacity.ui.dto.Ability;
+import de.egore911.capacity.ui.dto.Contract;
+import de.egore911.capacity.ui.dto.Employee;
+
+public class EmployeeCRUDTest extends AbstraceCRUDTest<Employee> {
+
+	@Override
+	protected Employee createFixture() {
+		Employee fixture = new Employee();
+		fixture.setName(UUID.randomUUID().toString());
+		fixture.setEmail(UUID.randomUUID().toString());
+		fixture.setAbilities(Collections.singleton(new Ability("Working")));
+		fixture.setColor("#00ff00");
+		Contract contract = new Contract();
+		contract.setVacationDaysPerYear(2);
+		fixture.setContract(contract);
+		return fixture;
+	}
+
+	@Override
+	protected String getPath() {
+		return "employee";
+	}
+
+	@Override
+	protected Class<Employee> getFixtureClass() {
+		return Employee.class;
+	}
+
+	@Override
+	protected void modifyFixture(Employee fixture) {
+		fixture.setName(UUID.randomUUID().toString());
+	}
+
+	@Override
+	protected void compareDtos(Employee fixture, Employee created) {
+		assertThat(created.getName(), equalTo(fixture.getName()));
+		assertThat(created.getEmail(), equalTo(fixture.getEmail()));
+		assertThat(created.getAbilities().size(), equalTo(fixture.getAbilities().size()));
+		assertThat(created.getAbilities().iterator().next().getName(), equalTo(fixture.getAbilities().iterator().next().getName()));
+	}
+
+}
