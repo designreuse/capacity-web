8c40af38b075190def3215d2d9b13229b6275290
Christoph Brill <egore911@gmail.com>
2016-04-19 00:12:05 +0200

    Add CRUD test for IcalImports

---
+++ src/test/java/de/egore911/capacity/ui/IcalImportCRUDTest.java
@@ -0,0 +1,41 @@
+package de.egore911.capacity.ui;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+import java.util.UUID;
+
+import de.egore911.capacity.ui.dto.IcalImport;
+
+public class IcalImportCRUDTest extends AbstraceCRUDTest<IcalImport> {
+
+	@Override
+	protected IcalImport createFixture() {
+		IcalImport fixture = new IcalImport();
+		fixture.setName(UUID.randomUUID().toString());
+		fixture.setUrl(UUID.randomUUID().toString());
+		return fixture;
+	}
+
+	@Override
+	protected String getPath() {
+		return "ical_import";
+	}
+
+	@Override
+	protected Class<IcalImport> getFixtureClass() {
+		return IcalImport.class;
+	}
+
+	@Override
+	protected void modifyFixture(IcalImport fixture) {
+		fixture.setUrl(UUID.randomUUID().toString());
+	}
+
+	@Override
+	protected void compareDtos(IcalImport fixture, IcalImport created) {
+		assertThat(created.getName(), equalTo(fixture.getName()));
+		assertThat(created.getUrl(), equalTo(fixture.getUrl()));
+	}
+
+}
