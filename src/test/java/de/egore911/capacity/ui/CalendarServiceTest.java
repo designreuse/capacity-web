15c1f9bcd1bdf856263cc1f0f90fab00cd541190
Christoph Brill <egore911@gmail.com>
2016-04-24 08:58:41 +0200

    Add test for the ui-calendar backend

--- src/test/java/de/egore911/capacity/ui/CalendarServiceTest.java
+++ src/test/java/de/egore911/capacity/ui/CalendarServiceTest.java
@@ -8,11 +8,14 @@ import static org.hamcrest.core.IsNull.notNullValue;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.core.GenericType;
 
 import org.junit.Test;
 
+import de.egore911.capacity.ui.dto.Event;
 import net.fortuna.ical4j.data.CalendarBuilder;
 import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
@@ -24,6 +27,14 @@ public class CalendarServiceTest extends AbstractUiTest {
 
 	@Test
 	public void testHolidays() throws IOException, ParserException {
+		List<Event> events = target("calendar/events/holidays").request().get(new GenericType<List<Event>>() {
+		});
+
+		assertThat(events, hasSize(4));
+	}
+
+	@Test
+	public void testHolidaysIcs() throws IOException, ParserException {
 		String ics = target("calendar/holidays.ics").request().get(String.class);
 
 		CalendarBuilder builder = new CalendarBuilder();
@@ -35,6 +46,14 @@ public class CalendarServiceTest extends AbstractUiTest {
 
 	@Test
 	public void testAbsences() throws IOException, ParserException {
+		List<Event> events = target("calendar/events/absences").request().get(new GenericType<List<Event>>() {
+		});
+
+		assertThat(events, hasSize(1));
+	}
+
+	@Test
+	public void testAbsencesIcs() throws IOException, ParserException {
 		String ics = target("calendar/absences.ics").request().get(String.class);
 
 		CalendarBuilder builder = new CalendarBuilder();
