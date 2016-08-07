8e239332532b27ca7a8b60aa28324e4a2ee3c86a
Christoph Brill <egore911@gmail.com>
2016-08-07 20:53:36 +0200

    Add methods hashing the users password
    Ideally this should be done on the client side

--- src/main/java/de/egore911/capacity/ui/rest/UserService.java
+++ src/main/java/de/egore911/capacity/ui/rest/UserService.java
@@ -1,11 +1,15 @@
 package de.egore911.capacity.ui.rest;
 
+import javax.annotation.Nonnull;
 import javax.ws.rs.Path;
 
 import de.egore911.capacity.persistence.dao.UserDao;
 import de.egore911.capacity.persistence.model.UserEntity;
 import de.egore911.capacity.persistence.selector.UserSelector;
 import de.egore911.capacity.ui.dto.User;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.shiro.codec.Hex;
+import org.apache.shiro.crypto.hash.Sha1Hash;
 
 @Path("user")
 public class UserService extends AbstractResourceService<User, UserEntity> {
@@ -30,4 +34,21 @@ public class UserService extends AbstractResourceService<User, UserEntity> {
 		return new UserDao();
 	}
 
+	@Override
+	public User create(User user) {
+		hashPassword(user);
+		return super.create(user);
+	}
+
+	@Override
+	public void update(@Nonnull Integer id, User user) {
+		hashPassword(user);
+		super.update(id, user);
+	}
+
+	private void hashPassword(User user) {
+		if (StringUtils.isNotEmpty(user.getPassword())) {
+			user.setPassword(new String(Hex.encode(new Sha1Hash(user.getPassword()).getBytes())));
+		}
+	}
 }
