60bc1cac6990ede058f368b6dc10389e75bb855b
Christoph Brill <egore911@gmail.com>
2016-10-18 21:49:23 +0200

    Ical Basic Auth: Allow administration only by users with the appropriate permission
    Also hide the password from anyone without the permission

--- src/main/java/de/egore911/capacity/ui/rest/IcalImportService.java
+++ src/main/java/de/egore911/capacity/ui/rest/IcalImportService.java
@@ -1,25 +1,34 @@
 package de.egore911.capacity.ui.rest;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
+import javax.annotation.Nonnull;
 import javax.persistence.EntityManager;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.apache.shiro.subject.Subject;
+import org.secnod.shiro.jaxrs.Auth;
 
 import de.egore911.capacity.persistence.dao.IcalImportDao;
 import de.egore911.capacity.persistence.model.IcalImportEntity;
+import de.egore911.capacity.persistence.model.Permission;
 import de.egore911.capacity.persistence.selector.IcalImportSelector;
 import de.egore911.capacity.ui.async.ProgressEndpoint;
 import de.egore911.capacity.ui.dto.IcalImport;
 import de.egore911.capacity.ui.dto.ImportResult;
 import de.egore911.capacity.ui.dto.Progress;
 import de.egore911.capacity.ui.exceptions.BadArgumentException;
+import de.egore911.capacity.ui.exceptions.NotAuthorizedException;
 import de.egore911.capacity.util.importer.IcalImporter;
 import de.egore911.capacity.util.listener.StartupListener;
 import de.egore911.persistence.util.EntityManagerUtil;
@@ -46,7 +55,59 @@ public class IcalImportService extends AbstractResourceService<IcalImport, IcalI
 	protected IcalImportDao getDao() {
 		return new IcalImportDao();
 	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<IcalImport> getAll(@Auth Subject subject) {
+		List<IcalImport> map = super.getAll(subject);
+		if (!subject.isPermitted(Permission.ADMIN_ICAL_IMPORTS.name())) {
+			for (IcalImport icalImport : map) {
+				icalImport.setPassword(null);
+			}
+		}
+		return map;
+	}
 
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public IcalImport create(IcalImport t, @Auth Subject subject) {
+		if (!subject.isPermitted(Permission.ADMIN_ICAL_IMPORTS.name())) {
+			throw new NotAuthorizedException();
+		}
+		return super.create(t, subject);
+	}
+
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public IcalImport getById(@PathParam("id") Integer id, @Auth Subject subject) {
+		IcalImport map = super.getById(id, subject);
+		if (!subject.isPermitted(Permission.ADMIN_ICAL_IMPORTS.name())) {
+			map.setPassword(null);
+		}
+		return map;
+	}
+
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void update(@PathParam("id") @Nonnull Integer id, IcalImport t, @Auth Subject subject) {
+		if (!subject.isPermitted(Permission.ADMIN_ICAL_IMPORTS.name())) {
+			throw new NotAuthorizedException();
+		}
+		super.update(id, t, subject);
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public void delete(@PathParam("id") @Nonnull Integer id, @Auth Subject subject) {
+		if (!subject.isPermitted(Permission.ADMIN_ICAL_IMPORTS.name())) {
+			throw new NotAuthorizedException();
+		}
+		super.delete(id, subject);
+	}
+	
 	@GET
 	@Path("import/{id}")
 	@Produces(MediaType.TEXT_PLAIN)
