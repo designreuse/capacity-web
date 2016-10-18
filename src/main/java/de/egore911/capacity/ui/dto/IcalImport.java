91f08c78e933007d5aca5fd29fc430c59faaaa0c
Christoph Brill <egore911@gmail.com>
2016-10-18 21:35:05 +0200

    Ical Basic Auth: Add new columns to DTO

--- src/main/java/de/egore911/capacity/ui/dto/IcalImport.java
+++ src/main/java/de/egore911/capacity/ui/dto/IcalImport.java
@@ -4,9 +4,16 @@ import java.time.LocalDateTime;
 
 public class IcalImport extends AbstractDto {
 
+	public enum Auth {
+		NONE, BASIC
+	}
+
 	private String name;
 	private String url;
 	private LocalDateTime lastImported;
+	private Auth auth;
+	private String username;
+	private String password;
 
 	public String getName() {
 		return name;
@@ -30,6 +37,30 @@ public class IcalImport extends AbstractDto {
 
 	public void setLastImported(LocalDateTime lastImported) {
 		this.lastImported = lastImported;
+	}
+
+	public Auth getAuth() {
+		return auth;
+	}
+
+	public void setAuth(Auth auth) {
+		this.auth = auth;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
